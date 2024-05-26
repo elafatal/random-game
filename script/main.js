d231ce03bb539cf563693f49fb9122f734f5095f
elafatal <vahediielahe@gmail.com>
2024-05-27 00:45:21 +0330

    start page

---
+++ script/main.js
@@ -0,0 +1,15 @@
+let gameStarted = false;
+let score = 0 ;
+
+const tagscore=document.getElementById("score"); 
+const easy = document.getElementById("easy");
+const average = document.getElementById("Average");
+const hard= document.getElementById("hard");
+
+
+function hideInputs() {
+    easy.style.display = 'none';
+    average.style.display = 'none';
+    hard.style.display = 'none';
+  }
+  
